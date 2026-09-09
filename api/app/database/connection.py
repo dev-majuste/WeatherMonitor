a7c2e0e559465b3350250321dfd7b6924b0113a2
Ricardo Majuste <ricardoalvesmajuste@gmail.com>
2026-09-09 01:38:51 -0300

    feat(api): add database session dependency

--- api/app/database/connection.py
+++ api/app/database/connection.py
@@ -16,3 +16,11 @@ DATABASE_URL = (
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
