5e6aa1bf89de67e8aa115632ed1281c193028117
Ricardo Majuste <ricardoalvesmajuste@gmail.com>
2026-09-09 01:42:06 -0300

    fix(api): fix measurement data types

--- api/app/models/measurement.py
+++ api/app/models/measurement.py
@@ -1,6 +1,13 @@
-from sqlalchemy import Boolean, Float, Integer
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from sqlalchemy import Float, Integer, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-from app.database import Base
+
+from app.database.database import Base
+
+def brazil_time():
+    return datetime.now(ZoneInfo("America/Sao_Paulo"))
 
 class Measurement(Base):
     __tablename__ = "measurements"
@@ -11,4 +18,8 @@ class Measurement(Base):
     pressure: Mapped[float] = mapped_column(Float, nullable=False)
     raindrop: Mapped[int] = mapped_column(Integer, nullable=False)
     luminosity: Mapped[int] = mapped_column(Integer, nullable=False)
-    timestamp: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=brazil_time,
+        nullable=False
+    )
--- api/app/schemas/measurement_schema.py
+++ api/app/schemas/measurement_schema.py
@@ -1,17 +1,22 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class MeasurementCreateSchema(BaseModel):
     temperature: float
     humidity: float
     pressure: float
     luminosity: int
-    raindrop: bool
+    raindrop: int
 
-class MeasurementResponse(BaseModel):
+
+class MeasurementResponseSchema(BaseModel):
     id: int
     temperature: float
     humidity: float
     pressure: float
     luminosity: int
-    raindrop: bool
-    date: str
+    raindrop: int
+    timestamp: datetime
+
+    class Config:
+        from_attributes = True
