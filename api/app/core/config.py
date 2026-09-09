7b287ae352f5896776a407508d87a8284d5ebd7d
Ricardo Majuste <ricardoalvesmajuste@gmail.com>
2026-09-09 01:32:49 -0300

    feat(api): add admin and device api keys

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -5,3 +5,6 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+
+DEVICE_API_KEY = os.getenv("DEVICE_API_KEY")
+ADMIN_API_KEY = os.getenv("ADMIN_API_KEY")
