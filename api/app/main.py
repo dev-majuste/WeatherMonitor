765ec0f08620f4a94be8a50fba1dd080b50a72a2
Ricardo Majuste <ricardoalvesmajuste@gmail.com>
2026-09-09 01:50:03 -0300

    feat(api): configure FastAPI app database and routes

--- api/app/main.py
+++ api/app/main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
 
+from app.database.connection import engine
+from app.database.database import Base
+
+from app.models.measurement import Measurement
+import app.models.api_key
+
+from app.routes.measurement_route import router as measurement_router
+from app.routes.api_key_route import router as api_key_router
+
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(measurement_router)
+app.include_router(api_key_router)
